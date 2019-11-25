d1da0f4318be5d41f2e5e5cffc579bf720bc51d1
Avnish Kumar <avnish@geninf.com>
2019-11-25 18:02:42 +0530

    Remove Unsed String, Add Data Annotation, Add Swagger

--- Repayabl/Startup.cs
+++ Repayabl/Startup.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +25,7 @@ namespace Repayabl
             services.AddSwaggerDocument(o =>
             {
                 o.Title = "JMS API";
-            });
+            }); 
         }
        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
