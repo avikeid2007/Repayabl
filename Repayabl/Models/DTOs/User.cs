493c5168a7edb6afb801fec28de05c3c3b09b235
Avnish Kumar <avnish@geninf.com>
2019-11-26 16:48:26 +0530

    Repo included and Login Complete

--- Repayabl/Models/DTOs/User.cs
+++ Repayabl/Models/DTOs/User.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Repayabl.Models.DTOs
 {
     public partial class User
     {
         public Guid Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public string UserName { get; set; }
+        [Required]
         public string Password { get; set; }
-        
+        public bool IsAdmin { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
     }
 }
---
+++ RepayablWebApp/Data/Repositories/BaseClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepayablWebApp.Data.Repositories
+{
+    public class BaseClient
+    {
+        public BaseClient()
+        {
+            BaseUrl = "http://localhost:62130";
+        }
+        public string BaseUrl { get; set; }
+    }
+}
