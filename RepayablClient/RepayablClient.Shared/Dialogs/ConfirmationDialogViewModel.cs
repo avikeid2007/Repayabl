a5594b426d43140523f3427518005f4c4607c3e8
Avnish Kumar <avnish@geninf.com>
2020-04-07 19:20:20 +0530

    Add Prism for Uno project

---
+++ RepayablClient/RepayablClient.Shared/Dialogs/ConfirmationDialogViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RepayablClient.Dialogs
+{
+    public class ConfirmationDialogViewModel : NotificationDialogViewModel
+    {
+        public ConfirmationDialogViewModel()
+        {
+            Title = "Confirmation";
+        }
+    }
+}
