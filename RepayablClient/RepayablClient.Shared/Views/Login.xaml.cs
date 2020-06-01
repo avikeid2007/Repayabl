e0a0f99cb309061bcaeed4d50f88d0e8abc52ab9
Avnish Kumar <avnish@geninf.com>
2020-06-01 16:25:09 +0530

    di for login viewmodel constructor

--- RepayablClient/RepayablClient.Shared/Views/Login.xaml.cs
+++ RepayablClient/RepayablClient.Shared/Views/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using RepayablClient.Shared.ViewModels;
+﻿using RepayablClient.Shared.Repositories;
+using RepayablClient.Shared.ViewModels;
 using Windows.UI.Xaml.Controls;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
@@ -8,12 +9,12 @@ namespace RepayablClient.Shared.Views
     public sealed partial class Login : UserControl
     {
         string graphAPIEndpoint = "https://graph.microsoft.com/v1.0/me";
-        public Login()
+        public Login(IUserClient userClient)
         {
 
             this.InitializeComponent();
 #if !NETFX_CORE
-            this.DataContext = new LoginViewModel();
+            this.DataContext = new LoginViewModel(userClient);
 #endif
         }
         //public void OnClick(object sender, object args)
